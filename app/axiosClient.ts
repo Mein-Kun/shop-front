d433749b01b153f65eb417224b68d412076154b8
Denis <kbs.pstu@gmail.com>
2023-12-17 16:34:19 +0500

    coommit 1

--- app/axiosClient.ts
+++ app/axiosClient.ts
@@ -2,8 +2,10 @@ import axios from 'axios'
 
 const instanse = axios.create({
   url: 'https://landmotors-client.onrender.com',
+  // url: 'http://localhost:3001',
   withCredentials: true,
   baseURL: 'https://landmotors-server.onrender.com',
+  // baseURL: 'http://localhost:3000',
   headers: { 'X-Requested-With': 'XMLHttpRequest' },
 })
 
