9d268f60cf7155f613a7c87efcde69163e44e118
Denis <kbs.pstu@gmail.com>
2024-01-06 14:28:44 +0500

    add axios

--- app/axiosClient.ts
+++ app/axiosClient.ts
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 const instanse = axios.create({
-  url: 'https://landmotors-client.onrender.com',
-  // url: 'http://localhost:3001',
+  // url: 'https://landmotors-client.onrender.com',
+  url: 'http://localhost:3001',
   withCredentials: true,
-  baseURL: 'https://landmotors-server.onrender.com',
-  // baseURL: 'http://localhost:3000',
+  // baseURL: 'https://landmotors-server.onrender.com',
+  baseURL: 'http://localhost:3000',
   headers: { 'X-Requested-With': 'XMLHttpRequest' },
 })
 
