c09caf92ca36c8515fb19f25c07978c64bfda50e
Denis <kbs.pstu@gmail.com>
2024-01-06 15:49:43 +0500

    delite response use

--- app/axiosClient.ts
+++ app/axiosClient.ts
@@ -6,7 +6,9 @@ const instanse = axios.create({
   withCredentials: true,
   // baseURL: 'https://landmotors-server.onrender.com',
   baseURL: 'http://localhost:3000',
-  headers: { 'X-Requested-With': 'XMLHttpRequest' },
+  headers: {
+    'X-Requested-With': 'XMLHttpRequest',
+  },
 })
 
 instanse.interceptors.request.use((config) => {
@@ -14,36 +16,36 @@ instanse.interceptors.request.use((config) => {
   return config
 })
 
-instanse.interceptors.response.use(
-  // в случае валидного accessToken ничего не делаем:
-  (config) => config,
-  // в случае просроченного accessToken пытаемся его обновить:
-  async (error) => {
-    // предотвращаем зацикленный запрос, добавляя свойство _isRetry
-    const originalRequest = { ...error.config }
-    originalRequest._isRetry = true
-    if (
-      // проверим, что ошибка именно из-за невалидного accessToken
-      error.response.status === 401 &&
-      // проверим, что запрос не повторный
-      error.config &&
-      !error.config._isRetry
-    ) {
-      try {
-        // запрос на обновление токенов
-        const resp = await instanse.get('/api/refresh')
-        // сохраняем новый accessToken в localStorage
-        localStorage.setItem('token', resp.data.accessToken)
-        // переотправляем запрос с обновленным accessToken
-        return instanse.request(originalRequest)
-      } catch (error) {
-        console.log('AUTH ERROR')
-      }
-    }
-    // на случай, если возникла другая ошибка (не связанная с авторизацией)
-    // пробросим эту ошибку
-    throw error
-  }
-)
+// instanse.interceptors.response.use(
+//   // в случае валидного accessToken ничего не делаем:
+//   (config) => config,
+//   // в случае просроченного accessToken пытаемся его обновить:
+//   async (error) => {
+//     // предотвращаем зацикленный запрос, добавляя свойство _isRetry
+//     const originalRequest = { ...error.config }
+//     originalRequest._isRetry = true
+//     if (
+//       // проверим, что ошибка именно из-за невалидного accessToken
+//       error.response.status === 401 &&
+//       // проверим, что запрос не повторный
+//       error.config &&
+//       !error.config._isRetry
+//     ) {
+//       try {
+//         // запрос на обновление токенов
+//         const resp = await instanse.get('/api/refresh')
+//         // сохраняем новый accessToken в localStorage
+//         localStorage.setItem('token', resp.data.accessToken)
+//         // переотправляем запрос с обновленным accessToken
+//         return instanse.request(originalRequest)
+//       } catch (error) {
+//         console.log('AUTH ERROR')
+//       }
+//     }
+//     // на случай, если возникла другая ошибка (не связанная с авторизацией)
+//     // пробросим эту ошибку
+//     throw error
+//   }
+// )
 
 export default instanse
